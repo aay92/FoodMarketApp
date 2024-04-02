81e9a36a505903bbdcfe487ca8d893159307e80b
Aleksey <aay92@yandex.ru>
2024-04-02 20:56:19 +0400

    Create base project, removed storyboard

---
+++ FoodMarketApp/FoodMarketApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FoodMarketApp
+//
+//  Created by Aleksey Alyonin on 02.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
+
