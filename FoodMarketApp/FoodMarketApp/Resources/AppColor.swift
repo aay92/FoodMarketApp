413d5a9ce06346b3301d58696f6ea6cb9b77c33c
Aleksey <aay92@yandex.ru>
2024-04-03 12:54:04 +0400

    added chenges in colors

--- FoodMarketApp/FoodMarketApp/Resources/AppColor.swift
+++ FoodMarketApp/FoodMarketApp/Resources/AppColor.swift
@@ -1,8 +1,10 @@
-//
-//  AppColor.swift
-//  FoodMarketApp
-//
-//  Created by Aleksey Alyonin on 03.04.2024.
-//
+import UIKit
 
-import Foundation
+enum AppColors {
+    static let accentOrange = UIColor().hex(0xD35400)
+    static let grey = UIColor().hex(0xECF0F1)
+    static let black = UIColor().hex(0x000000)
+    static let background = UIColor().hex(0xE5E5E5)
+    static let white = UIColor.white
+}
+
