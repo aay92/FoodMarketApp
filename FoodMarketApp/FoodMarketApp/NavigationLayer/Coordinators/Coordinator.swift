2a6ed1f37c048fb6de6d10598f56c013b4f82d18
Aleksey <aay92@yandex.ru>
2024-04-09 20:17:30 +0400

    added coordinator protocol

---
+++ FoodMarketApp/FoodMarketApp/NavigationLayer/Coordinators/Coordinator.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+enum CoordinatorType {
+    case app
+    case onboarding
+    case home
+    case order
+    case list
+    case profile
+}
+
+protocol CoordinatorProtocol: AnyObject {
+    var childCoordinators: [CoordinatorProtocol] { get set }
+    var type: CoordinatorType { get }
+    var navigationController: UINavigationController? { get set }
+    var finishDelegate: CoordinatorFinishDelegate? { get set }
+    
+    func start()
+    func finish()
+}
+
+extension CoordinatorProtocol {
+    func addChildCoordinator(childCoordinator: CoordinatorProtocol) {
+        childCoordinators.append(childCoordinator)
+    }
+    
+    func removeChildCoordinator(childCoordinator: CoordinatorProtocol){
+        childCoordinators = childCoordinators.filter {$0 !== childCoordinator } 
+    }
+}
+
+protocol CoordinatorFinishDelegate: AnyObject {
+    func coordinatorDidFinish(childCoordinators: CoordinatorProtocol)
+}
